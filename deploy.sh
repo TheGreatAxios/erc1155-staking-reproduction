28ab99882fff2fdbb79cc2ee5c96b0d7fe3db654
TheGreatAxios <sawyer@dirtroad.dev>
2025-01-19 23:06:13 -0800

    Add Deploy Script + Test Deployment

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -eE
+
+forge script ./script/ERC1155StakeandBurnCityBuilderXpert.s.sol --legacy --slow --broadcast --rpc-url https://testnet.skalenodes.com/v1/juicy-low-small-testnet
--- script/ERC1155StakeandBurnCityBuilderXpert.s.sol
+++ script/ERC1155StakeandBurnCityBuilderXpert.s.sol
@@ -2,17 +2,25 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {TestToken} from "../src/mocks/TestToken.sol";
+
 import {ERC1155StakeandBurnCityBuilderXpert} from "../src/ERC1155StakeandBurnCityBuilderXpert.sol";
 
 contract ERC1155StakeandBurnCityBuilderXpertScript is Script {
-    ERC1155StakeandBurnCityBuilderXpert public stakingContract;
+    TestToken public testToken;
+    ERC1155StakeandBurnCityBuilderXpert public builder;
 
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
 
-        stakingContract = new ERC1155StakeandBurnCityBuilderXpert();
+        testToken = new TestToken();
+        builder = new ERC1155StakeandBurnCityBuilderXpert();
+
+        testToken.setApprovalForAll(address(builder), true);
+        builder.stakeERC1155(address(testToken), 1, 1);
 
         vm.stopBroadcast();
     }
